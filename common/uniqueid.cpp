e1afb29bed8fe6d575a25a554ba28adfb5e29595
Richard Herveille <richard.herveille@roalogic.com>
2023-05-03 16:03:36 -0700

    Initial commit

---
+++ common/uniqueid.cpp
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//    Base Class for Classes with UniqueIDs                        //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//             Copyright (C) 2023 Roa Logic BV                     //
+//             www.roalogic.com                                    //
+//                                                                 //
+//     This source file may be used and distributed without        //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//      THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY        //
+//   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED     //
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     //
+//   FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR        //
+//   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,           //
+//   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES      //
+//   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE     //
+//   GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR          //
+//   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    //
+//   LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT    //
+//   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT    //
+//   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           //
+//   POSSIBILITY OF SUCH DAMAGE.                                   //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+/*!
+ * @file uniqueid.cpp
+ * @author Richard Herveille
+ * @brief UniqueId base class
+ * @version 0.1
+ * @date 3-may-2023
+ * @copyright See beginning of file
+ */
+
+
+#include "uniqueid.hpp"
+
+
+//Reset globalId
+unsigned long RoaLogic::common::cUniqueId::_globalId = 0;
+
---
+++ common/uniqueid.hpp
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//    Base Class for Classes with UniqueIDs                        //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//             Copyright (C) 2023 Roa Logic BV                     //
+//             www.roalogic.com                                    //
+//                                                                 //
+//     This source file may be used and distributed without        //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//      THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY        //
+//   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED     //
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     //
+//   FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR        //
+//   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,           //
+//   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES      //
+//   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE     //
+//   GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR          //
+//   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF    //
+//   LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT    //
+//   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT    //
+//   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           //
+//   POSSIBILITY OF SUCH DAMAGE.                                   //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+/*!
+ * @file uniqueid.hpp
+ * @author Richard Herveille
+ * @brief UniqueId base class
+ * @version 0.1
+ * @date 3-may-2023
+ * @copyright See beginning of file
+ */
+
+#ifndef UNIQUEID_HPP
+#define UNIQUEID_HPP
+
+#define UNIQUEID_HPP
+
+
+namespace RoaLogic
+{
+namespace common
+{
+
+    /**
+     * @class cUniqueId
+     * @author Richard Herveille
+     * @brief Template base class for a classes with a unique ID
+     * @version 0.1
+     * @date 3-may-2023
+     *
+     * @details This is a template base class for classes with unique IDs
+     * 
+     */
+    class cUniqueId
+    {
+        private:
+            static unsigned long _globalId;  //Global ID counter
+            const  unsigned long _myId;      //This instance's ID
+
+        protected:
+            /**
+             * @brief Construct a new cUniqueId object
+             */
+            cUniqueId() : _myId(_globalId++) {};
+
+        public:
+            /**
+             * @brief return the UniqueId
+             */
+            unsigned long id() { return _myId; };
+    };
+
+}
+}
+
+#endif
